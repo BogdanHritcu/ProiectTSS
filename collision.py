7d05b3654242fde8204ef147588e8397e4c900c1
BogdanHritcu <hritcubogdan99@gmail.com>
2022-04-25 23:05:32 +0300

    collision for circles and AABB

--- collision.py
+++ collision.py
@@ -1,3 +1,5 @@
+from cmath import sqrt
+
 def clamp(x, left, right):
     return max(min(x, right), left)
 
@@ -5,6 +7,15 @@ class Vec:
     def __init__(self, x = None, y = None):
         self.x = x
         self.y = y
+
+    def len(self):
+        return sqrt(self.x * self.x + self.y * self.y)
+    
+    def len2(self):
+        return self.x * self.x + self.y * self.y
+
+    def __neg__(self):
+        return Vec(-self.x, -self.y)
 
     def __add__(self, other):
         return Vec(self.x + other.x, self.y + other.y)
@@ -31,6 +42,27 @@ class Circle:
         self.pos = Vec(x, y)
         self.r = r
 
+def collision_r_r(rect1, rect2):
+    collision_x = rect1.pos.x + rect1.size.x > rect2.pos.x and rect2.pos.x + rect2.size.x > rect1.pos.x
+    collision_y = rect1.pos.y + rect1.size.y > rect2.pos.y and rect2.pos.y + rect2.size.y > rect1.pos.y
+
+    return collision_x and collision_y
+
+def collision_c_c(circle1, circle2):
+    d = circle1.pos - circle2.pos
+    r = circle1.r - circle2.r
+    
+    return d.len2() < r**2
+
 def collision_r_c(rect, circle):
-    rect_center = rect.pos + rect.size / 2
+    rect_half = rect.size / 2
+    rect_center = rect.pos + rect_half
     d = circle.pos - rect_center
+    closest = rect_center + Vec(clamp(d.x, -rect_half.x, rect_half.x), clamp(d.y, -rect_half.y, rect_half.y))
+
+    diff = closest - circle.pos
+
+    return diff.len2() < circle.r**2
+
+def collision_c_r(circle, rect):
+    return collision_r_c(rect, circle)
